cd8bcfaadbf7639029afa90f099bf2ecfba25731
AbdullahBera <kucukabdullahbera@gmail.com>
2021-11-02 10:13:16 -0700

    Day 3

---
+++ hackerrank_30daychallenge/30daychallenge-day3.py
@@ -0,0 +1,16 @@
+if __name__ == '__main__':
+    N = int(input().strip())
+
+    if N % 2 != 0:
+        print('Weird')
+    
+    else: 
+        if N >= 2 and N <= 5:
+            print("Not Weird")
+
+        if N >=6 and N <= 20:
+            print("Not Weird")
+        
+        if N > 20: 
+            print("Not Weird")
+        
