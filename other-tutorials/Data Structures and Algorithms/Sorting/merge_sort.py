b5309609dd2d0595a7fab9e38523ac52030874aa
yaejinpark <yaejin91@gmail.com>
2021-11-03 21:27:44 -0700

    merge sort practice file setup

---
+++ other-tutorials/Data Structures and Algorithms/Sorting/merge_sort.py
@@ -0,0 +1,21 @@
+# Practice based on Brian Faure's video
+
+# Import random integer array class file
+import sys
+sys.path.append('..')
+from random_integer_arrays import IntegerArray
+
+# This function is where the arrays get merged when called
+def merge(left_arr,right_arr):
+	return arr
+
+# This is where the arrays get separated into individual elements and calls the merge function
+def merge_sort(arr):
+	return merge(left,right)
+
+testArray1 = IntegerArray(5,10).create_array()
+testArray2 = IntegerArray(8,20).create_array()
+testArray3 = IntegerArray(1,3).create_array()
+print(testArray1)
+print(testArray2)
+print(testArray3)
