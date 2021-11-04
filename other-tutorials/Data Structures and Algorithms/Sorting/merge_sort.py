313df40f89202b66e21d2448c5dd76810e0c9a9e
yaejinpark <yaejin91@gmail.com>
2021-11-03 22:39:43 -0700

    merge_sort function populated

--- other-tutorials/Data Structures and Algorithms/Sorting/merge_sort.py
+++ other-tutorials/Data Structures and Algorithms/Sorting/merge_sort.py
@@ -7,11 +7,30 @@ from random_integer_arrays import IntegerArray
 
 # This function is where the arrays get merged when called
 def merge(left_arr,right_arr):
-	return arr
+	res = []
+
+
+
+	return res
 
 # This is where the arrays get separated into individual elements and calls the merge function
 def merge_sort(arr):
-	return merge(left,right)
+	# Partition array until each element are in their separate arrays
+	if len(arr) <= 1:
+		return arr
+
+	mid = len(arr)//2
+
+	# Partition happening with updated mid
+	left_arr,right_arr = merge_sort(arr[:mid]),merge_sort(arr[mid+1:])
+	merged = merge(left_arr,right_arr)
+
+	print(f"merge_sort left array: {left_arr}")
+	print(f"merge_sort right array: {right_arr}")
+	print(f"merged arrays: {merged}")
+
+	return merged
+
 
 testArray1 = IntegerArray(5,10).create_array()
 testArray2 = IntegerArray(8,20).create_array()
