06a43ae52c83ae391592064def51cb09092079d8
yaejinpark <yaejin91@gmail.com>
2021-11-15 19:38:33 -0800

    added another tutorial source

---
+++ other-tutorials/Data Structures and Algorithms/YouTube-codebasics/binary_tree.py
@@ -0,0 +1,11 @@
+# Searching elements is fast because every time I'm looking for an element, I'm halving the search base (right and left)
+# That's why the search complexity is O(log n)
+
+# Two ways of searching in a BST - Breadth First Search (BFS) and Depth First Search (DFS).
+
+# BFS
+
+# DFS
+# In order traversal
+# Pre order traversal
+# Post order traversal
