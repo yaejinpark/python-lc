6fcc64dbc0b04d7d29bf70e9d367c90d109fc3da
Yae Jin <yaejin91@gmail.com>
2022-01-25 18:30:36 -0800

    solved intro to window problem. Looks like a long road ahead of me but whatever

--- other-tutorials/Patterns/sliding_windows/1-intro.py
+++ other-tutorials/Patterns/sliding_windows/1-intro.py
@@ -7,7 +7,7 @@ Find the average of all subarrays of K contiguous elements in a given array.
 Expected Output: [2.2, 2.8, 2.4, 3.6, 2.8]
 """
 
-# Brute Force (My attempt - no sliding windows)
+# Brute Force (My attempt - no sliding windows), Time Complexity of O(N*k)
 
 def brute_force(arr,k):
 	output = []
@@ -20,9 +20,25 @@ def brute_force(arr,k):
 			output.append(elem_sum/k)
 	return output
 
+# Better way - consider the subarrays as windows, add/subtract windows and reuse the sum of 4 overlapping elements.
+
+def windows_avg(arr,k):
+	output = []
+	window_start,window_sum = 0, 0.0
+
+	for window_end in range(len(arr)):
+		window_sum += arr[window_end]
+
+		if window_end >= k-1: # window_end starts at 0, so just having it = k will make the sum have sum of 6 elements
+			output.append(window_sum/k)
+			window_sum -= arr[window_start]
+			window_start += 1 # start of the window shifts by 1
+	return output
+
 
 test_arr = [1, 3, 2, 6, -1, 4, 1, 8, 2]
 k_val = 5
 
-print(brute_force(test_arr,k_val))
+# print(brute_force(test_arr,k_val))
+print(windows_avg(test_arr,k_val))
 
