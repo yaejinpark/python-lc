0e65e79850878fe81787f4e1abc096871b598b83
Matt Hardy <corporalhardy@gmail.com>
2022-08-24 11:52:56 -0700

    updated rpsls with Jin's tweaks, working now

--- codewars/rock_paper_scissors_lizard_spock.py
+++ codewars/rock_paper_scissors_lizard_spock.py
@@ -11,11 +11,11 @@
 # Rock crushes scissors
 
 def rpsls(input1, input2):
-    if (input1 == "scissors" and input2 == ("paper" or "lizard")) or \
-    (input1 == "paper" and input2 == ("rock" or "spock")) or \
-    (input1 == "rock" and input2 == ("lizard" or "scissors")) or \
-    (input1 == "lizard" and input2 == ("spock" or "paper")) or \
-    (input1 == "spock" and input2 == ("scissors" or "rock")):
+    if (input1 == "scissors" and (input2 == "paper" or input2 == "lizard")) or \
+    (input1 == "paper" and (input2 == "rock" or input2 =="spock")) or \
+    (input1 == "rock" and (input2 == "lizard" or input2 =="scissors")) or \
+    (input1 == "lizard" and (input2 == "spock" or input2 =="paper")) or \
+    (input1 == "spock" and (input2 == "scissors" or input2 == "rock")):
         return "Player 1 Won!"
     elif input1 == input2:
         return "Draw!"
