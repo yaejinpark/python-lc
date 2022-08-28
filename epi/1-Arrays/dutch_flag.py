0e80dbaba39909d24e110c7c5bd99e0ed475a453
Yae Jin Park <yaejinpark@Yaes-MacBook-Pro.local>
2022-08-27 20:18:10 -0700

    added some practice problems from EPI book

--- epi/1-Arrays/dutch_flag.py
+++ epi/1-Arrays/dutch_flag.py
@@ -1,16 +1,24 @@
 """
-Write a program that takes an array A and an index i into A, and rearrange the elements
-such that all elements less than A[i] (pivot) appear first, followed by elements equal to the pivot,
-followed by elements greater than the pivot.
-
-Example: Given an index 3 for array [1,0,1,1,2,1,1], [1,1,0,1,1,1,2] is a valid answer.
+Write a program that takes an array A and an index i rnto A, 
+and rearranges the elements such that all elements less than A[r] (the "pivot") appear first, 
+followed by elements equal to the pivot, followed by elements greater than the pivot.
 """
-def dfp_brute(arr, index):
-    pivot = arr[index]
 
+def dutch_flag(arr,pivot):
+    left_arr, right_arr = [],[]
 
+    # Append all elements smaller than pivot to left_arr, bigger to right_arr
+    for elem in arr:
+        if arr[pivot] >= elem:
+            left_arr.append(elem)
+        else:
+            right_arr.append(elem)
+    print(left_arr,right_arr)
+    # Extend doesn't work since None is returned
+    # The book mentioned using recursion... might look into it
+    return left_arr + right_arr
 
-    return arr
+test1 = [0,1,2,0,2,1,1]
+test1_pivot = 3
 
-test_arr = [8,5,3,4,1,7,6,2]
-dfp_brute(test_arr,3)
+print(dutch_flag(test1,test1_pivot))
