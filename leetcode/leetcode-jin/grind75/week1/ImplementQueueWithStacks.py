242684ce7a2597d54697f1e5d2b291d0ed9258b2
Jin Park <yaejin91@gmail.com>
2023-01-05 22:41:58 -0800

    solved a few problems for grind 75

---
+++ leetcode/leetcode-jin/grind75/week1/ImplementQueueWithStacks.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/implement-queue-using-stacks/description/
+
+class MyQueue:
+
+    def __init__(self):
+        self.s1 = []
+        self.s2 = []
+
+    def push(self, x: int) -> None:
+        self.s1.append(x)
+
+    def pop(self) -> int:
+        self.peek()
+        return self.s2.pop()
+
+    def peek(self) -> int:
+        if not self.s2:
+            while self.s1:
+                self.s2.append(self.s1.pop())
+        return self.s2[-1]
+
+    def empty(self) -> bool:
+        return not self.s1 and not self.s2
+
+queue = MyQueue()
+queue.push(1)
+queue.push(2)
+print(queue.peek())
+print(queue.pop())
+queue.empty()
